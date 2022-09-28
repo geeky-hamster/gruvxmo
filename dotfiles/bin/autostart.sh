7bd4dfbb3a8ba9bb3014139c2c93eac642a235fa
geeky-hamster <110613129+geeky-hamster@users.noreply.github.com>
2022-09-28 15:37:16 +0530

    Add files via upload

---
+++ dotfiles/bin/autostart.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+## Autostart Programs
+
+# Kill already running process
+_ps=(picom dunst ksuperkey mpd xfce-polkit xfce4-power-manager)
+for _prs in "${_ps[@]}"; do
+	if [[ `pidof ${_prs}` ]]; then
+		killall -9 ${_prs}
+	fi
+done
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Polkit agent
+/usr/lib/xfce-polkit/xfce-polkit &
+
+# Enable power management
+xfce4-power-manager &
+
+# Enable Super Keys For Menu
+ksuperkey -e 'Super_L=Alt_L|F1' &
+ksuperkey -e 'Super_R=Alt_L|F1' &
+
+# Restore wallpaper
+#hsetroot -cover /usr/share/backgrounds/nordic-wallpapers-git/sola.png
+hsetroot -cover ~/.xmonad/wall/b2.png
+#hsetroot -cover ~/.xmonad/wall/a3.png
+#hsetroot -cover ~/.xmonad/wall/gruvbox_a1.png
+
+# Lauch notification daemon
+~/.xmonad/bin/xmodunst.sh
+
+# Lauch polybar
+~/.xmonad/bin/launch.sh
+
+# Lauch compositor
+~/.xmonad/bin/xmocomp.sh
+
+# Start mpd
+exec mpd &
